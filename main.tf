18c847a3bbc5f537cb3d8cf9cea4fc6f2539fb23
Dor Levi <dor.levi.dor@gmail.com>
2025-07-13 11:20:50 +0200

    Initial version

---
+++ main.tf
@@ -0,0 +1,45 @@
+locals {
+	tags = merge(var.tenx_lambda_user_supplied_tags, {
+		terraform-module			= "tenx-aws-lambda"
+		terraform-module-version	= "v0.1.0"
+		managed-by					= "tenx-terraform"
+	})
+
+	lambda_bootstrap_args = [
+		"logConfig", var.tenx_lambda_log_config
+	]
+}
+
+module "tenx_pipeline_lambda_function" {
+	source	= "terraform-aws-modules/lambda/aws"
+	version = "8.0.1"
+
+	function_name	= var.tenx_lambda_name
+	description		= var.tenx_lambda_description
+
+	create_package	= false
+
+	s3_existing_package = {
+		bucket = "log10x-engine-lambda"
+		key    = "releases/${var.tenx_lambda_image_version}/tenx-lambda.zip"
+	}
+
+	handler       = "com.log10x.ext.lambda.run.RunLambda::handleRequest"
+	runtime       = "java21"
+
+	cloudwatch_logs_retention_in_days	= var.tenx_lambda_logs_retention_days
+	timeout			= var.tenx_lambda_timeout
+	memory_size		= var.tenx_lambda_mem
+
+	environment_variables = {
+		TENX_LICENSE				= "${var.tenx_lambda_license_key}"
+		TENX_DEFAULT_APP			= "${var.tenx_lambda_app}"
+		TENX_DEFUALT_BOOTSTRAP		= jsonencode(local.lambda_bootstrap_args)
+		TENX_DEFAULT_CONFIG			= jsonencode(var.tenx_lambda_options)
+		TENX_MODULES				= "/var/task/modules"
+		TENX_CONFIG					= "/var/task/config"
+		JAVA_TOOL_OPTIONS			= "-Djdk.httpclient.allowRestrictedHeaders=host"
+	}
+
+	tags	= local.tags
+}
---
+++ variables.tf
@@ -0,0 +1,66 @@
+variable "tenx_lambda_user_supplied_tags" {
+	description	= "Tags supplied by the user to populate to all generated resources"
+	type		= map(string)
+	default		= {}
+}
+
+variable "tenx_lambda_name" {
+	description	= "Set the name of the lambda function, defaults to 'my-10x-lambda'"
+	type		= string
+	default		= "my-10x-lambda"
+}
+
+variable "tenx_lambda_description" {
+	description	= "Set the description of the lambda function, defaults to 'Lambda running a 10x engine'"
+	type		= string
+	default		= "Lambda running a 10x engine"
+}
+
+variable "tenx_lambda_image_version" {
+	description = "Set the version of the docker image to use for this lambda, defaults to 'latesl'"
+	type        = string
+	default     = "latest"
+}
+
+variable "tenx_lambda_mem" {
+	description	= "Set the memory size of the lambda function, defaults to 1024mb"
+	type		= number
+	default		= 1024
+}
+
+variable "tenx_lambda_timeout" {
+	description	= "Set the timeout of the lambda function, defaults to 900 seconds"
+	type		= number
+	default		= 900
+}
+
+variable "tenx_lambda_logs_retention_days" {
+	description	= "Set the CloudWatch logs retention days policy of the lambda function, defaults to 7"
+	type		= number
+	default		= 7
+}
+
+variable "tenx_lambda_app" {
+	description	= "Set the 10x application to run by default with this lambda"
+	type		= string
+	default		= ""
+}
+
+variable "tenx_lambda_options" {
+	description	= "Set the options map to pass into the lambda each execution."
+	type		= map(any)
+	default		= {}
+}
+
+variable "tenx_lambda_license_key" {
+	description = "Log10x account license key"
+	type		= string
+	sensitive	= true
+	default		= "NO-LICENSE"
+}
+
+variable "tenx_lambda_log_config" {
+	description = "Config file for the lambda log4j"
+	type		= string
+	default		= "log4j2-lambda.yaml"
+}
